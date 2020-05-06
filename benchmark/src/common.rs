cbb1a5467129cdd59166d52ed001673883fdbd00
Zongmin Lei <leizongmin@gmail.com>
2020-05-06 23:37:32 +0800

    test: benchmark for map & sorted_list

---
+++ benchmark/src/common.rs
@@ -0,0 +1,54 @@
+use std::env::temp_dir;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+use rand::Rng;
+
+use cedar::database::Database;
+
+#[allow(dead_code)]
+pub fn benchmark_test_case<F>(title: &str, count: usize, mut f: F)
+    where
+        F: FnMut(usize),
+{
+    println!("start {}...", title);
+    let start = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_millis();
+    f(count);
+    let end = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_millis();
+    let spent = end - start;
+    let qps = count as f64 / (spent as f64 / 1000 as f64);
+    println!("{}, spent={}ms, count={}, qps={}", title, spent, count, qps as i64);
+}
+
+#[allow(dead_code)]
+pub fn get_random_database_path() -> String {
+    let r: i32 = rand::thread_rng().gen();
+    let path = temp_dir().as_path().join(format!(
+        "test-cedar-rs-{}-{}",
+        SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_millis(),
+        r
+    ));
+    String::from(path.to_str().unwrap())
+}
+
+#[allow(dead_code)]
+pub fn open_database() -> Database {
+    let path = get_random_database_path();
+    Database::destroy(&path).unwrap();
+    open_database_with_path(&path)
+}
+
+#[allow(dead_code)]
+pub fn open_database_with_path(path: &str) -> Database {
+    let db = Database::open(path).unwrap();
+    println!("open database: {}", db.path);
+    db
+}
---
+++ benchmark/src/main.rs
@@ -0,0 +1,70 @@
+use cedar::encoding::get_score_bytes;
+
+pub mod common;
+
+fn main() {
+    // test_map();
+    test_sorted_list();
+}
+
+fn test_map() {
+    let mut db = common::open_database_with_path(common::get_random_database_path().as_str());
+    let key = "hello_map";
+    let value = "hello, world".as_bytes();
+    let count = 10_0000;
+    let fields: Vec<String> = (0..count).map(|i| format!("field_{}", i)).collect();
+
+    common::benchmark_test_case("map_put", count, |_| {
+        for f in &fields {
+            db.map_put(key, f, value).unwrap();
+        }
+    });
+    common::benchmark_test_case("map_get", count, |_| {
+        for f in &fields {
+            db.map_get(key, f).unwrap();
+        }
+    });
+    common::benchmark_test_case("map_count", count, |count| {
+        for _ in 0..count {
+            db.map_count(key).unwrap();
+        }
+    });
+    common::benchmark_test_case("map_delete", count, |_| {
+        for f in &fields {
+            db.map_delete(key, f).unwrap();
+        }
+    });
+}
+
+fn test_sorted_list() {
+    let mut db = common::open_database_with_path(common::get_random_database_path().as_str());
+    let key = "hello_sorted_list";
+    let count = 1_0000;
+    let items: Vec<(Vec<u8>, String)> = (0..count).map(|i| (get_score_bytes(i as i64), format!("field_{}", i))).collect();
+
+    common::benchmark_test_case("sorted_list_add", count, |_| {
+        for (score, value) in &items {
+            db.sorted_list_add(key, score.as_slice(), value.as_bytes()).unwrap();
+        }
+    });
+    common::benchmark_test_case("sorted_list_count", count, |count| {
+        for _ in 0..count {
+            db.sorted_list_count(key).unwrap();
+        }
+    });
+    common::benchmark_test_case("sorted_list_left_pop", count, |_| {
+        for (score, value) in &items {
+            db.sorted_list_left_pop(key, Some(score.as_slice())).unwrap();
+        }
+    });
+    common::benchmark_test_case("sorted_list_add", count, |_| {
+        for (score, value) in &items {
+            db.sorted_list_add(key, score.as_slice(), value.as_bytes()).unwrap();
+        }
+    });
+    common::benchmark_test_case("sorted_list_right_pop", count, |_| {
+        for (score, value) in &items {
+            db.sorted_list_right_pop(key, None).unwrap();
+        }
+    });
+}
