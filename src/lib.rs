96a926d1c6faaa09c8c325792180ecc7e8ab8485
Zongmin Lei <leizongmin@gmail.com>
2020-05-18 11:52:27 +0800

    fix: exports Options

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,5 @@ extern crate lazy_static;
 mod database;
 pub mod encoding;
 
-pub use database::Database;
+pub use database::{Database, Options};
 pub use encoding::{BytesComparableScore, KeyMeta, KeyType};
