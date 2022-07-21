c519c47e683022c6509018af5974c382478cfc77
github-action-benchmark <github@users.noreply.github.com>
2022-07-21 16:15:04 +0000

    add Benchmark (cargo) benchmark result for bf1a644c6ece52b9f9d9c806c3bd304ebb0e68e8

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1658420104029,
+  "repoUrl": "https://github.com/leizongmin/simpledb",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "leizongmin@gmail.com",
+            "name": "LEI Zongmin",
+            "username": "leizongmin"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bf1a644c6ece52b9f9d9c806c3bd304ebb0e68e8",
+          "message": "Merge pull request #18 from rmw-lib/master\n\ncargo +nightly clippy",
+          "timestamp": "2022-07-21T23:47:43+08:00",
+          "tree_id": "33a9525885ca48daff07fc8af96dcb4ad15f769b",
+          "url": "https://github.com/leizongmin/simpledb/commit/bf1a644c6ece52b9f9d9c806c3bd304ebb0e68e8"
+        },
+        "date": 1658420103351,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "map_put",
+            "value": 13865,
+            "range": "± 3755",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "map_get",
+            "value": 10837,
+            "range": "± 241",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
